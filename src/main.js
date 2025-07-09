ab2dd24bb4849cda4c073a8ce03e3359841bbed8
projeck-web <larasgann@Gmail.com>
2025-07-09 21:41:07 +0700

    test

--- src/main.js
+++ src/main.js
@@ -7,3 +7,10 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.mount('#app')
+
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
